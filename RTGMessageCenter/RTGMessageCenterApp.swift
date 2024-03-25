39ca9e5c9a3fe6a18c129f5701315f207e0e34f6
Kyle Watson <dev.kyle.watson@gmail.com>
2024-03-25 11:10:11 -0400

    Initial Commit

---
+++ RTGMessageCenter/RTGMessageCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RTGMessageCenterApp.swift
+//  RTGMessageCenter
+//
+//  Created by Kyle Watson on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct RTGMessageCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
